c676e8baec194227a9fdf195f5b63436d0b8287a
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2024-06-11 12:01:58 -0600

    Add error handling around plugin option retrieval

--- smd_inventory.py
+++ smd_inventory.py
@@ -56,19 +56,26 @@ class InventoryModule(BaseInventoryPlugin):
         # declared in DOCUMENTATION (retrievable via `get_option()`)
         self._read_config_data(path)
 
-        # Query the smd server to retrieve its component list
-        # TODO: Load smd groups as Ansible groups?
-        result = get_smd(
-                self.get_option('smd_server'), "State/Components",
-                filter_by=json_loads(self.get_option('filter_by')),
-                access_token=self.get_option('access_token'))
+        try:
+            # Query the smd server to retrieve its component list
+            # TODO: Load smd groups as Ansible groups?
+            result = get_smd(
+                    self.get_option('smd_server'), "State/Components",
+                    filter_by=json_loads(self.get_option('filter_by')),
+                    access_token=self.get_option('access_token'))
 
-        # Make each component from smd available to ansible
-        for component in result['Components']:
-            nid_name = 'nid' + str(component['NID']).zfill(self.get_option('nid_length'))
-            self.inventory.add_host(nid_name)
-            # Load a host variable with the state from smd, in case it's needed later
-            self.inventory.set_variable(nid_name, 'smd_component', component)
+            # Make each component from smd available to ansible
+            for component in result['Components']:
+                nid_name = 'nid' + str(component['NID']).zfill(self.get_option('nid_length'))
+                self.inventory.add_host(nid_name)
+                # Load a host variable with the state from smd, in case it's needed later
+                self.inventory.set_variable(nid_name, 'smd_component', component)
+
+        except KeyError as e:
+            # Handle unset config optons
+            raise AnsibleParserError(
+                    f"Please ensure that all required options in config file \"{path}\" are set",
+                    e) from e
 
 
 def get_smd(host: str, endpoint: str, filter_by: dict,
